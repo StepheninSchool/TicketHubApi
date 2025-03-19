44744a74f307aa24a84374c5a981f94ae2ad92d1
StepheninSchool <w0488750@campus.nscc.ca>
2025-03-19 16:10:25 -0300

    Add project files.

---
+++ Controllers/TicketsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+
+namespace TicketHubApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly ILogger<TicketsController> _logger;
+        private readonly IConfiguration _configuration;
+        public TicketsController(ILogger<TicketsController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from Tickets controller - GET()");
+        }
+        [HttpPost]
+        public IActionResult Post(Ticket ticket) //stystem automatically will bind to correspoding field in object
+        {
+
+            //validation
+            if (string.IsNullOrEmpty(ticket.FirstName) || string.IsNullOrEmpty(ticket.LastName))
+            {
+                return BadRequest("First and Last name is required");
+            }
+            //save to database
+            return Ok("Hello " + ticket.FirstName + " from Tickets controller - POST()");
+        }
+    }
+}
---
+++ Ticket.cs
@@ -0,0 +1,8 @@
+﻿namespace TicketHubApi
+{
+    public class Ticket
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
